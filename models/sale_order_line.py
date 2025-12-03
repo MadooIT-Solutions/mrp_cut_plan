f60dbd765435241d9c1baa7cc8c76311b481f8d1
rodrigo <rodrigomadu85@gmail.com>
2025-12-03 13:32:56 -0300

    [16.0] Removido quantity do compute_blue_m2

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -124,7 +124,7 @@ class SaleOrderLine(models.Model):
             else:
                 line.blue_m3 = 0
 
-    @api.depends('blue_I','blue_II','blue_I_uom','blue_II_uom','quantity','blue_advance','blue_wall','blue_advance_uom','blue_wall_uom','product_id','blue_h','blue_h_uom')
+    @api.depends('blue_I','blue_II','blue_I_uom','blue_II_uom','blue_advance','blue_wall','blue_advance_uom','blue_wall_uom','product_id','blue_h','blue_h_uom')
     def _compute_blue_m2(self):
         meter_uom = self.env.ref('uom.product_uom_meter')
         for line in self:
